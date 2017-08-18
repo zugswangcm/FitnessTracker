4d5c4def93bb25cba9fda0a1c67bfefd14661dc2
Ashwin Singh <nitromekid@hotmail.com>
2017-08-18 15:30:08 -0400

    Created new activity, linked it to tracker_layout, began onClick method.

--- app/src/main/java/com/example/owner/tracker/MainActivity.java
+++ app/src/main/java/com/example/owner/tracker/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.owner.tracker;
 
+import android.support.constraint.solver.ArrayLinkedVariables;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    ArrayList<String> arrayList;
+    ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.listView);
+        arrayList = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(arrayAdapter);
+    }
+    public void onClick(View v) {
+
     }
 }
